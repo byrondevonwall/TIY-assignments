1aa5f180a6eddd6a16e89b19bf2346265915cd49
Byron Wall <byrondevonwall@gmail.com>
2016-02-09 10:42:58 -0500

    Add second incomplete and busted exercise to inclass26.js

--- inclass29.js
+++ inclass29.js
@@ -6,3 +6,17 @@ for(num = 0; num <= 20; num++){
     console.log(num + " is odd");
   }
 }
+
+//exercise 2
+
+
+function verbing(verb){
+  if(verb.length >= 3){
+    return(verb + "ing");
+  }
+  else if(verb.length > 3 && verb.length < 0){
+    return(verb)
+  }
+}
+console.log(verb)
+var verb = "go"
