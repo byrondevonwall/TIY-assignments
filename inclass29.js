b3e64f4e2a675d6873c4606161239ffbd8cfc705
Byron Wall <byrondevonwall@gmail.com>
2016-02-09 11:01:56 -0500

    polish inclass26.js

--- inclass29.js
+++ inclass29.js
@@ -9,14 +9,16 @@ for(num = 0; num <= 20; num++){
 
 //exercise 2
 
+var verb = "swim"
+// var ing = "ing"
+// var ly = "ly"
 
 function verbing(verb){
-  if(verb.length >= 3){
-    return(verb + "ing");
+  if(verb.length >= 2){
+    return(verb.concat("ing"));
   }
-  else if(verb.length > 3 && verb.length < 0){
-    return(verb)
+  else if(verb.length < 2 && verb.length > 0){
+    return(verb);
   }
 }
 console.log(verb)
-var verb = "go"
