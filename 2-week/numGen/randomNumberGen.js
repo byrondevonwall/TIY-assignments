a05bfcf7a0078ecf2f912669b1b82b922a87a374
Byron Wall <byrondevonwall@gmail.com>
2016-02-09 18:16:45 -0500

    Add functional number generator and reading list

--- 2-week/numGen/randomNumberGen.js
+++ 2-week/numGen/randomNumberGen.js
@@ -1,27 +1,21 @@
-var input = 3;
+function rand() {
+   var a = document.getElementById('a').value;
+  //  var b = document.getElementById('b').value;
 
-var randNum = Math.floor(Math.random() * 10) + 1;
-
-console.log(randNum)
-
-if(randNum === input){
-  console.log("You've guessed the correct number, you win!");
-}
-else if(randNum > input){
-  console.log("Oh no! The number you've guessed is too high!");
-}
-else if(randNum < input){
-  console.log("Oh no!  The number you've guessed is too low!");
-}
-// function getRandomNumber( lower, upper ) {
-//   if(isNaN(lower) || isNaN(upper)){
-//   throw new Error('Both arguments must be numbers')
-//   }
-//   return Math.floor(Math.random() * (upper - lower + 1)) + lower;
-// }
-//
-// console.log( getRandomNumber( 'nine', 24 ) );
-// console.log( getRandomNumber( 1, 100 ) );
-// console.log( getRandomNumber( 200, 'five hundred' ) );
-// console.log( getRandomNumber( 1000, 20000 ) );
-// console.log( getRandomNumber( 50, 100 ) );
+  //  var userNum = parseInt(a) + parseInt(b);
+   var userNum = parseInt(a);
+   var randNum = Math.floor(Math.random() * 10) + 1;
+  //  alert(userNum);
+  //  alert(randNum);
+   if(randNum === userNum){
+       document.write("You've guessed the correct number, you win!");
+     }
+     else if(randNum < userNum){
+       document.write("Oh no! The number you've guessed is too high!");
+     }
+     else if(randNum > userNum){
+       document.write("Oh no!  The number you've guessed is too low!");
+     }
+     document.write("your number is: " + userNum);
+     document.write("the random number is: " + randNum);
+ }
