38b02a19572260cdd32ec7cb97b6d38a70546e35
Byron Wall <byrondevonwall@gmail.com>
2016-02-10 10:46:13 -0500

    Add second inclass exercise to inclass210

--- 2-week/in-class/inclass210.js
+++ 2-week/in-class/inclass210.js
@@ -9,3 +9,19 @@ function calculateAge(birth, current){
 calculateAge(1985, 2016);
 calculateAge(1990, 2076);
 calculateAge(1895, 1947);
+
+
+var favoriteRecipe = []
+
+function createRecipe(title, servings, ingredients){
+  favoriteRecipe.push({
+    title: Title,
+    servings: Servings,
+    ingredients: Ingredients
+  });
+}
+createRecipe("curry", 4, ["coconut milk", "green curry paste", "bell peppers", "winter squash", "chicken", "love", "heat"] );
+
+console.log(favoriteRecipe.title);
+console.log("Serves " + favoriteRecipe.servings);
+console.log("Ingredients: " + favoriteRecipe.ingredients);
