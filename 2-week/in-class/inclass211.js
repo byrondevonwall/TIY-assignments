82880669f0fa48cca29d13b72b9065e9c2e50d8c
Byron Wall <byrondevonwall@gmail.com>
2016-02-11 10:55:13 -0500

    Add challenge 2 to inclass211.js

--- 2-week/in-class/inclass211.js
+++ 2-week/in-class/inclass211.js
@@ -1,7 +1,30 @@
 function calculateSupply(age, dailyAmt){
-  var supply = age*(dailyAmt*365);
+  var maxAge = 100;
+  var supply = (maxAge - age) * (dailyAmt*365);
   console.log("You will need " + supply + " to last you to the ripe old age of " + age);
 }
 calculateSupply(45, 20);
 calculateSupply(100, 5);
 calculateSupply(85, 8);
+
+
+//challenge 2
+
+var favMovie = []
+
+function movieArray(title, duration, stars){
+  favMovie.push({
+    title: title,
+    duration: duration,
+    stars: stars
+  });
+}
+
+movieArray("The Big Lebowski", 117, ["Jeff Bridges", "John Goodman", "Steve Buscemi", "Philip Seymour Hoffman"]);
+
+// console.log(favMovie);
+
+function printMovie(){
+  console.log(favMovie[0].title + " lasts for " + favMovie[0].duration + " minutes.  Stars: " + favMovie[0].stars);
+}
+printMovie(favMovie);
