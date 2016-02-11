ba47a62875b719955bbc5121aa80360c3fdd331a
Byron Wall <byrondevonwall@gmail.com>
2016-02-11 10:58:36 -0500

    Add for loop to 2/11 challenge 2

--- 2-week/in-class/inclass211.js
+++ 2-week/in-class/inclass211.js
@@ -20,11 +20,18 @@ function movieArray(title, duration, stars){
   });
 }
 
-movieArray("The Big Lebowski", 117, ["Jeff Bridges", "John Goodman", "Steve Buscemi", "Philip Seymour Hoffman"]);
+movieArray("The Big Lebowski", 117, ["Jeff Bridges", " John Goodman", " Steve Buscemi", " Philip Seymour Hoffman"]);
 
 // console.log(favMovie);
 
 function printMovie(){
   console.log(favMovie[0].title + " lasts for " + favMovie[0].duration + " minutes.  Stars: " + favMovie[0].stars);
 }
+
+function printMovies(){
+  for (mv = 0; mv < favMovie.length; mv++){
+    console.log(favMovie[mv].title + " lasts for " + favMovie[mv].duration + " minutes.  Stars: " + favMovie[mv].stars);
+  }
+}
 printMovie(favMovie);
+printMovies(favMovie);
