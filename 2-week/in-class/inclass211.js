0201e4403b4d3e3876d3e232e13d18a49a2e925c
Byron Wall <byrondevonwall@gmail.com>
2016-02-11 10:00:53 -0500

    Add pretty phrase to console output

--- 2-week/in-class/inclass211.js
+++ 2-week/in-class/inclass211.js
@@ -1,6 +1,6 @@
 function calculateSupply(age, dailyAmt){
   var supply = age*(dailyAmt*365);
-  console.log(supply);
+  console.log("You will need " + supply + " to last you to the ripe old age of " + age);
 }
 calculateSupply(45, 20);
 calculateSupply(100, 5);
