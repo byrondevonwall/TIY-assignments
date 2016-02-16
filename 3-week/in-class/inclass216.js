742fd5add3cf2bbb247ac4ae3340c39086fb6a8f
Byron Wall <byrondevonwall@gmail.com>
2016-02-16 11:00:49 -0500

    change up inclass216 a bit

--- 3-week/in-class/inclass216.js
+++ 3-week/in-class/inclass216.js
@@ -1,19 +1,13 @@
 function pluralize(noun, number){
   var num = parseInt(number)
-  if(num === 1){
+  if(num === 1 || noun === "sheep" || noun === "geese" || noun === "moose" || noun === "fish" || noun ==="mice"){
     console.log(num + " " + noun);
   }
-  else if(noun === "sheep"){
-    console.log(num + " " + noun);
-  }
-  else if(noun === "geese"){
-    console.log(num + " " + noun);
-  }
-  else if(noun === "moose"){
-    console.log(num + " " + noun);
-  }
+  // else if(noun.lastChild.value === "s"){
+  //   console.log("This noun is plural");
   else{
     console.log(num + " " + noun + "s")
+    // console.log(noun.lastChild.value)
   }
 }
 
@@ -23,3 +17,5 @@ pluralize("rabbit", 1);
 pluralize("sheep", 10);
 pluralize("geese", 30);
 pluralize("moose", 300);
+//
+// pluralize('cats', 2);
