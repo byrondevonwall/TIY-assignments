a735d779df9b3dc5d1cfca1aa5f2f8356e281965
Byron Wall <byrondevonwall@gmail.com>
2016-02-17 10:33:05 -0500

    Add more languages to 217 inclass challenge 1

--- 3-week/in-class/inclass217.js
+++ 3-week/in-class/inclass217.js
@@ -8,6 +8,15 @@ function helloWorld(lang){
   else if (lang === "hindi"){
     console.log("नमस्ते दुनिय!!!");
   }
+  else if (lang === "kr"){
+    console.log("안녕하세요 세계!!!");
+  }
+  else if (lang === "ru"){
+    console.log("Привет мир!!!");
+  }
+  else if (lang === "en"){
+    console.log("Hello world, english is boring");
+  }
   else{
     console.log("pick another language");
   }
@@ -16,3 +25,6 @@ function helloWorld(lang){
 helloWorld("cn");
 helloWorld("jp");
 helloWorld("hindi");
+helloWorld("kr");
+helloWorld("ru");
+helloWorld("en");
