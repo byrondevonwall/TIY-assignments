ce325b4f526df10f2b9bb6adc4b713fcc2026c2a
Byron Wall <byrondevonwall@gmail.com>
2016-02-22 10:49:27 -0500

    Add inclass222.js first exercise

---
+++ 4-week/inclass/inclass222.js
@@ -0,0 +1,22 @@
+function greaterNum(arg1, arg2){
+  var num1 = parseInt(arg1);
+  var num2 = parseInt(arg2);
+  if(num1 > num2){
+    console.log("The greater number of " + num1 + " and " + num2 + " is " + num1);
+  }
+  else if(num2 > num1){
+    console.log("The greater number of " + num1 + " and " + num2 + " is " + num2);
+  }
+  else if(num2 === num1){
+    console.log(num1 + " is equal to " + num2);
+  }
+  else if(arg1 === NaN || arg2 === NaN){
+    console.log("Please insert numbers to compare, not things that are not numbers.  KTHXBAI")
+  }
+}
+
+greaterNum(10, 8);
+greaterNum(11, 3);
+greaterNum(100, 82);
+greaterNum(17, 17000);
+greaterNum("pandas", "kittens");
