3f9154b17d8ea2e4ad15705bf265959691ffb2c8
Byron Wall <byrondevonwall@gmail.com>
2016-02-25 10:41:52 -0500

    add suffixes

--- 4-week/inclass/inclass225.js
+++ 4-week/inclass/inclass225.js
@@ -1,9 +1,17 @@
-var favStuff = ["cats", "kittens", "felines", "chat", "gato", "猫", "🐈"];
+var favStuff = ["cats", "kittens", "felines", "chats", "gatos", "猫", "🐈"];
 
 for(var c = 0; c<=favStuff.length-1; c++){
-  console.log("my "+[c+1]+" choice is "+favStuff[c]);
+  if([c+1]==1){
+  console.log("my "+[c+1]+"st choice is "+favStuff[c]);
+} else  if([c+1]==2){
+  console.log("my "+[c+1]+"nd choice is "+favStuff[c]);
+}else  if([c+1]==3){
+  console.log("my "+[c+1]+"rd choice is "+favStuff[c]);
+} else if([c+1] >= 4){
+  console.log("my "+[c+1]+"th choice is "+favStuff[c]);
 }
-
-// favStuff.each(function(){
+}
 //
+// favStuff.each(function(c){
+// console.log("my "+[c+1]+" choice is "+favStuff[c]);
 // })
