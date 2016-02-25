f24a0f1caa881dd6a34b29656ed3842189a6417a
Byron Wall <byrondevonwall@gmail.com>
2016-02-25 10:37:18 -0500

    Add inclass225.js first challenge

---
+++ 4-week/inclass/inclass225.js
@@ -0,0 +1,9 @@
+var favStuff = ["cats", "kittens", "felines", "chat", "gato", "猫", "🐈"];
+
+for(var c = 0; c<=favStuff.length-1; c++){
+  console.log("my "+[c+1]+" choice is "+favStuff[c]);
+}
+
+// favStuff.each(function(){
+//
+// })
