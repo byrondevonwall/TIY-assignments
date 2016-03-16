844cae2958e99159fde6121719b81df21fbf6123
Byron Wall <byrondevonwall@gmail.com>
2016-03-16 10:31:37 -0400

    inclass316

--- 7-week/inclass316.js
+++ 7-week/inclass316.js
@@ -1,12 +1,22 @@
-function sortinLetters(str){
-  var strArr = []
-  var trimmed= str.trim()
-  for(i=1; i<trimmed.length+1; i++){
-  strArr.push(trimmed.slice(i-1,i));
+// function sortinLetters(str){
+//   var strArr = []
+//   var trimmed= str.trim()
+//   for(i=1; i<trimmed.length+1; i++){
+//   strArr.push(trimmed.slice(i-1,i));
+//
+//   }
+//     strArr.sort();
+//     console.log(strArr.join(''));
+// }
+//
+// sortinLetters('hello world');
 
-  }
-    strArr.sort();
-    console.log(strArr.join(''));
+function longest_country_name(arr){
+  console.log(arr);
+  arr.sort(function (a, b) { return b.length - a.length })[0];
+  console.log(arr);
+  console.log(arr[0]);
 }
 
-sortinLetters('hello world');
+
+longest_country_name(['Abyssinia', 'Bechuanaland', 'Grain Coast', 'Songhay', 'Stellaland', 'Kingdom of Tungning', 'British North Borneo', 'Gorkhali', 'Kampuchea', 'Mustang, Kingdom of', 'Inini', 'Confœderatio Helvetica', 'Pridnestrovia', 'Spitzbergen'])
